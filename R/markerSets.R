3c58764509c1dc9f3dd6f731c9e3a6db66ada889
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2025-05-01 20:45:26 +0200

    Reorder marker sets

--- R/markerSets.R
+++ R/markerSets.R
@@ -6,21 +6,19 @@
 #'
 #' Kits included:
 #'
+#' * `codis`: Expanded CODIS core loci (FBI/NIST), 21 autosomal STR markers.
 #' * `fusion6c`: PowerPlex® Fusion 6C (Promega), 23 autosomal markers.
-#' * `globalfiler`: GlobalFiler™ PCR Amplification Kit (Applied Biosystems), 21
-#' autosomal markers.
-#' * `codis`: Expanded CODIS core loci (FBI/NIST), 20 autosomal STR markers.
+#' * `globalfiler`: GlobalFiler™ PCR Amplification Kit (Applied Biosystems),
+#' 21 autosomal markers.
+#' * `hdplex`: HDplex STR Kit (Qiagen), 9 autosomal markers.
 #' * `sureid`: SureID® 23comp Human STR Identification Kit (Health Gene
 #' Technologies), 23 autosomal markers.
 #' * `sureid27`: Combined SureID® 23comp and SureID® PathFinder Plus; 26
 #' autosomal markers.
-#' * `hdplex`: HDplex STR Kit (Qiagen), 9 autosomal markers.
-#'
 #'
 #' @format A named list of 6 character vectors of marker names.
 #'
 #' @examples
 #' markerSets$fusion6c
-#' markerSets$globalfiler
 #'
 "markerSets"
