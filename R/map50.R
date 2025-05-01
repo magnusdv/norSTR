b22f436ddcec073c42b205b0267d196367c46618
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2025-05-01 19:39:54 +0200

    Add dataset `map50`

---
+++ R/map50.R
@@ -0,0 +1,9 @@
+#' Built-in linkage map
+#'
+#' A map providing the genetic positions (in centiMorgans) of each STR marker
+#' featured in this package.
+#'
+#' @format A data frame with 50 rows and 4 columns: `Marker`, `Chr`, `cM`,
+#'   `Kit`.
+#'
+"map50"
---
+++ data-raw/map50.R
@@ -0,0 +1,5 @@
+
+map50 = readxl::read_excel("data-raw/linkageMap50.xlsx")
+map50
+usethis::use_data(map50, overwrite = TRUE)
+
